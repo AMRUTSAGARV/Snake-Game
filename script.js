997263f8e8335e838801136dc74b1a60ed7db468
Amrut Sagar <amrutsagar06@gmail.com>
2022-01-04 23:51:16 +0530

    snake game

--- script.js
+++ script.js
@@ -39,11 +39,11 @@ function moveOutcomes() {
 
 //deals with the snake hitting border and snake hitting self
 if ( 
-    (currentSnake[0] + width >= (width*width) && direction === width ) ||
+    (currentSnake[0] + width >= (width * width) && direction === width ) ||
     (currentSnake[0] % width === width -1 && direction ===1) ||
     (currentSnake[0] % width === 0 && direction === -1) ||
-    (currentSnake[0] - width < 0 && direction === -width)
-    // squares[currentSnake[0] + direction].classList.contains('snake')
+    (currentSnake[0] - width < 0 && direction === -width) ||
+    squares[currentSnake[0] + direction].classList.contains('snake')
 ) {
     return clearInterval(interval)
 }
